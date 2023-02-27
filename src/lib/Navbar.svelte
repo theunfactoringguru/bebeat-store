ddff0a70478fd6ccd2cf72b57897968cc1f2f101
Mariano Hurtado de Mendoza <theunfactoringguru@gmail.com>
2023-02-26 20:25:10 -0600

    Added navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,36 @@
+<nav>
+  <h2>Bebeat Store</h2>
+  <form>
+    <input type="text" placeholder="Search for artist or kit name..." />
+    <input type="submit" value="Search" />
+  </form>
+  <div class="account-buttons">    
+    <button>Sign Up</button>
+    <button>Log In</button>
+  </div>
+</nav>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  nav {
+    display: flex;
+    justify-content: space-between;
+    background-color: darkblue;
+    padding: 2rem;
+  }
+
+  form {
+    align-self: center;
+  }
+
+  .account-buttons {
+    align-self: center;
+  }
+
+  h2 {
+    color: white;
+  }
+</style>
