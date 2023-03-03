90415f99dbb613df2012c2db4d8026057e03378b
Mariano Hurtado de Mendoza <theunfactoringguru@gmail.com>
2023-03-03 12:09:13 -0600

    Added new beat modal

--- src/routes/Profile.svelte
+++ src/routes/Profile.svelte
@@ -1,5 +1,6 @@
 <script>
   import BeatCard from "./components/BeatCard.svelte";
+  import BeatUploadModal from "./components/BeatUploadModal.svelte";
 
   let user = {
     artistName: 'John Doe',
@@ -10,23 +11,44 @@
       {name: "Rap beats", beatNames: ["Name 1", "Name 2","Name 3"]},
     ]
   }
+
+  let showBeatsUploadModal = false;
+  let songAmount = 0;
 </script>
 
-<img src="profile.jpg" width="8%" alt="Profile Image"/>
-<div>
-  <h3>{user.artistName}</h3>
-  {#each user.beats as beat}
-    <BeatCard
-      artistName={user.artistName}
-      beatPlaylistName={beat.name}
-      beatList={beat.beatNames}
-      setArtistName={false}
-    />
-  {/each}
+<div class="parent">
+  <img src="profile.jpg" width="8%" alt="Profile Image"/>
+  <div>
+    <h3>{user.artistName}</h3>
+    <button on:click={() => showBeatsUploadModal = true}>+ New Beat</button>
+    {#each user.beats as beat}
+      <BeatCard
+        artistName={user.artistName}
+        beatPlaylistName={beat.name}
+        beatList={beat.beatNames}
+        setArtistName={false}
+      />
+    {/each}
+  </div>
+
+  <BeatUploadModal bind:showBeatsUploadModal>
+    <h2 slot="header">Upload Beats</h2>
+    <div class="info-form">
+      <form>
+        <input type="text" placeholder="Beat Playlist Name"/>
+        <br>
+        <input type="number" placeholder="Song Quantity" bind:value={songAmount}/>
+        <br>
+        {#each Array(songAmount) as _, i}
+          <input type="text" placeholder="Song name..."/>
+        {/each}
+      </form>
+    </div>
+  </BeatUploadModal>
 </div>
 
 <style>
-  * {
+  .parent {
     margin-top: 5rem;
     padding-left: 10rem;
     padding-right: 10rem;
@@ -34,5 +56,13 @@
 
   img {
     border-radius: 10px;
+  }
+
+  div > button {
+    width: fit-content;
+    padding: 30px 10px;
+    font-family: 'Ubuntu', 'Courier New', Courier, monospace;
+    color: aliceblue;
+    background-color: darkblue;
   }
 </style>
